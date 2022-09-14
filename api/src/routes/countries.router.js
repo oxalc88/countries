89249c4bb3b075c408d41570284f9b9adc124db8
Rolando Oxalc <rolando.oxalc@gmail.com>
2022-09-14 13:12:29 -0500

    Revisado el get all funcionando correctamente

--- api/src/routes/countries.router.js
+++ api/src/routes/countries.router.js
@@ -1,12 +1,45 @@
 const { Router } = require('express');
+const { Country } = require('../db');
+//const { Sequelize } = require("sequelize");
+
 
 const router = Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        const categories = await service.find();
-        res.json(categories);
+        const countries = await Country.findAll()
+        res.send(countries);
+    } catch (error) {
+        // res.status(404).json({
+        //     error: error.message
+        // })
+        next(error);
+    }
+});
+
+router.get('/:idPais', async (req, res, next) => {
+    const { idPais } = req.params
+    try {
+        const countryByID = await Country.findByPk(idPais);
+        res.json(countryByID);
+    } catch (error) {
+        // res.status(404).json({
+        //     error: error.message
+        // })
+        next(error);
+    }
+});
+
+router.get('/', async (req, res, next) => {
+    const { name } = req.query;
+    try {
+        const countryByName = await Country.findOne({
+            where: { name }
+        });
+        res.json(countryByName);
     } catch (error) {
         next(error);
     }
 });
+
+module.exports = router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const countriesRouter = require('./countries.router')
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/countries', countriesRouter)
 
 
 module.exports = router;
